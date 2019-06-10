e10e6617e18d3c7777ac6df76d448f03f730d9a6
Uriel Hayon <uri.a77aque@gmail.com>
2019-06-10 00:13:22 -0300

    Remove redux thunk

--- src/model/index.js
+++ src/model/index.js
@@ -1,10 +1,9 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, compose } from 'redux';
 
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(
   reducers,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers()
 );
